6f954caaed49af7f9fea48ef62988b7004208514
Joaribero <joaribero_la12@hotmail.com>
2021-01-30 20:08:10 -0300

    Show images in index

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -11,24 +11,29 @@ ctrl.index = (req,res) => {
 };
 
 ctrl.create = (req,res) => {
+    //Creo una funcion recursiva, para no repetir nombres.
     const saveImage = async () => {
         const imgUrl = randomNumber();
         const images = await Image.find({fileName: imgUrl});
         
+        //Si el nombre random ya existe, vuelvo a ejectura la funcion hasta que no se vuelva a repetir
         if (images.lenght > 0) {           
             saveImage();
 
-        } else {
+        } else { //el nombre no existe, continuo con el guardado.
             console.log(imgUrl);
         
             const imageTempPath = req.file.path;
             const ext = path.extname(req.file.originalname).toLowerCase();
             const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
 
+            //Valido que la extensión del archivo sea de una imagen.
             if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
                 
+                //Modifico la ruta de la imagen, de temp a upload
                 await fs.rename(imageTempPath,targetPath);
                 
+                //Creo el objeto Image para mongo y después poder almacenarlo.
                 const newImg = new Image({
                     title: req.body.title,
                     description: req.body.description,
@@ -39,7 +44,8 @@ ctrl.create = (req,res) => {
                 
                 //res.redirect('/images');
                 res.send('Works')
-            }else {
+                
+            }else { //No es una imagen, elimino lo que se haya subido y respondo error.
                 
                 await fs.unlink(imageTempPath);
                 
@@ -47,6 +53,7 @@ ctrl.create = (req,res) => {
             }                   
         }             
     }
+    //El create inicia acá, ejecutando la funcion recursiva.
     saveImage();
 
 };
