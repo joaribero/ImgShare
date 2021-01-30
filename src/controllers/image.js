342cda35a3f2044f7d589f4321a42baf4abea640
Joaribero <joaribero_la12@hotmail.com>
2021-01-30 18:45:20 -0300

    Add create function

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -10,25 +10,45 @@ ctrl.index = (req,res) => {
 
 };
 
-ctrl.create = async (req,res) => {
-    const imgUrl = randomNumber();
-    console.log(imgUrl);
-    const imageTempPath = req.file.path;
-    const ext = path.extname(req.file.originalname).toLowerCase();
-    const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
+ctrl.create = (req,res) => {
+    const saveImage = async () => {
+        const imgUrl = randomNumber();
+        const images = await Image.find({fileName: imgUrl});
+        
+        if (images.lenght > 0) {           
+            saveImage();
 
-    if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
-        await fs.rename(imageTempPath,targetPath);
-        const newImg = new Image({
-            title: req.body.title,
-            description: req.body.description,
-            fileName: imgUrl+ ext
-        });
-        const imagedSave = await newImg.save();
-        console.log(imagedSave);
-    };
+        } else {
+            console.log(imgUrl);
+        
+            const imageTempPath = req.file.path;
+            const ext = path.extname(req.file.originalname).toLowerCase();
+            const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
 
-    res.send('Works')
+            if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+                
+                await fs.rename(imageTempPath,targetPath);
+                
+                const newImg = new Image({
+                    title: req.body.title,
+                    description: req.body.description,
+                    fileName: imgUrl+ ext
+                });
+                
+                const imagedSave = await newImg.save();
+                
+                //res.redirect('/images');
+                res.send('Works')
+            }else {
+                
+                await fs.unlink(imageTempPath);
+                
+                res.status(500).json({error: 'Only images are allowed'});
+            }                   
+        }             
+    }
+    saveImage();
+
 };
 
 ctrl.like = (req,res) => {
